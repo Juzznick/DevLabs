680cf1a8974d2dd17f7fe123737633fb36f9f0bb
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-13 23:58:15 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-        <ul className='flex justify-around'>
+    <div className='flex justify-evenly mt-3'>
+        <h1 className='w-2/5 flex justify-center'>Hello</h1>
+        <ul className='flex justify-around w-2/5'>
           <li className=' '>HOME</li>
           <li>EVENTS</li>
           <li>GALLERY</li>

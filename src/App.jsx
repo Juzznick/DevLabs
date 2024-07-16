e5693164df9fca84d50808fa36e106c5e4a7e172
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-16 23:48:46 +0530

    hover effects

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 function App() {
 
   return (
-    <div className='flex h-1 justify-evenly mt-5'>
-        <ul className='flex flex-col justify-around w-2/5'>
-          <li className='font-bold text-2xl'>Home</li>
-          <li className='font-bold text-2xl'>Events</li>
-          <li className='font-bold text-2xl'>Gallery</li>
-          <li className='font-bold text-2xl'>Contact</li>
+    <div className='flex h-screen justify-evenly mt-5'>
+        <ul className='flex flex-col justify-center w-2/5'>
+          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>HOME</li>
+          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>ACTIVITIES</li>
+          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>GALLERY</li>
+          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>HIREUS</li>
+          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>LOGIN</li>
         </ul>
     </div>
   )

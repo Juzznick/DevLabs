fd8a0d981fb5d621979cb8adb9309731b853a2ea
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-18 23:06:56 +0530

    color

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ function App() {
 
   return (
     <div className='flex h-screen justify-evenly mt-5'>
-        <h1 className=' text-6xl  fixed top-5 right-5'>HOME</h1>
+        <h1 className=' text-6xl text-cyan-300 fixed top-5 right-5'>HOME</h1>
         <ul className='flex flex-col justify-center w-2/5'>
-          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>HOME</li>
-          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>ACTIVITIES</li>
-          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>GALLERY</li>
-          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>HIREUS</li>
-          <li className='font-bold text-2xl hover:text-cyan-600 cursor-pointer'>LOGIN</li>
+          <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>HOME</li>
+          <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>ACTIVITIES</li>
+          <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>GALLERY</li>
+          <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>HIREUS</li>
+          <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>LOGIN</li>
         </ul>
     </div>
   )

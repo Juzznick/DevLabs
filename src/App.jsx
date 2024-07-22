7aca3da44e3a1253a70588ae6e9896da6102bb72
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-22 23:43:37 +0530

    removed the home image

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ function App() {
   return (
     <div className='flex h-screen justify-evenly mt-5'>
         <h1 className=' text-6xl text-cyan-300 fixed top-5 right-5'>HOME</h1>
-        <ul className='flex flex-col justify-center w-2/5'>
+        <ul className='flex justify-between w-3/5'>
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>HOME</li>
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>ACTIVITIES</li>
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>GALLERY</li>
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>HIREUS</li>
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>LOGIN</li>
         </ul>
-        <img src='img1.png' className='w-1/4 h-1/4 fixed top-1/3 right-1/4'/>
     </div>
   )
 }

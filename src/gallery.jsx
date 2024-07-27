e4e76b81fa366e071fd56c8075bf36185fe420ff
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-27 22:34:45 +0530

    added gallery comp

---
+++ src/gallery.jsx
@@ -0,0 +1,6 @@
+function Gallery(){
+    return(
+        <h1 className="bold flex justify-center text-2xl pt-24">Gallery</h1>
+    );
+}
+export default Gallery
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Activities from './activities.jsx'
+import Gallery from './gallery.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

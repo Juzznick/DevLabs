a82234b68be08e48a4e4e3dbd76e1a0f09868f4a
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-29 00:07:30 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className='flex flex-col h-screen items-center pt-5'>
-        <h1 className=' text-6xl text-cyan-300 fixed bottom-5 right-5'>HOME</h1>
+        <h1 className=' text-6xl fixed bottom-5 right-5'>DevLabs</h1>
         <ul className='flex justify-between w-3/5 items-center'>
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>HOME</li>
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>ACTIVITIES</li>
@@ -12,12 +12,12 @@ function App() {
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>HIREUS</li>
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>LOGIN</li>
         </ul>
-        <div className='flex justify-around w-screen h-2/3'>
-          <div className='w-2/3 flex justify-center items-center'>
-            <h1>We help you shape your Future.</h1>
+        <div className='flex justify-center items-center w-screen h-screen'>
+          <div className='w-2/3 flex justify-center items-center text-8xl pl-24'>
+            <h1 className='flex'>We help you shape your Future.</h1>
           </div>
-          <div className='w-2/3 flex justify-center items-center'>
-            <h1>We help you shape your Future.</h1>
+          <div className='w-1/3 flex justify-center items-center'>
+            <img className='w-134' src='img1.png'/>
           </div>
         </div>
     </div>

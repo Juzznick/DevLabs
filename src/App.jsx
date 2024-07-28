c22a329294d7f6930235e3b8046609e46439b675
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-28 23:35:06 +0530

    colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className='flex h-screen justify-evenly mt-5'>
+    <div className='flex h-screen justify-evenly pt-5'>
         <h1 className=' text-6xl text-cyan-300 fixed bottom-5 right-5'>HOME</h1>
         <ul className='flex justify-between w-3/5'>
           <li className='font-bold text-2xl hover:text-cyan-300 cursor-pointer'>HOME</li>

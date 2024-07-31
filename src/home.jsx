73f72710e972fec5fa2119c0afcb9c11288dd81c
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-31 23:54:06 +0530

    home

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-function App() {
+function Home() {
 
   return (
     <div className='flex flex-col h-screen items-center pt-5'>
@@ -23,4 +23,4 @@ function App() {
   )
 }
 
-export default App
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Home from './home.jsx'
 import Activities from './activities.jsx'
 import Gallery from './gallery.jsx'
 import Login from './login.jsx'
@@ -8,6 +8,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <Home/>
   </React.StrictMode>,
 )

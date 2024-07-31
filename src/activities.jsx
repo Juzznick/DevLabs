a9fd957a5aa8fc5be08d3b55598d8c8b2b86ce59
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-31 23:58:04 +0530

    activities

--- src/activities.jsx
+++ src/activities.jsx
@@ -1,6 +1,9 @@
 function Activities(){
     return(
-        <h1 className="bold flex justify-center text-2xl pt-24">Activities</h1>
+        <div>   
+            <h1 className="bold flex justify-center text-2xl pt-24">Activities</h1>
+        </div>
+        
     );
 }
 export default Activities

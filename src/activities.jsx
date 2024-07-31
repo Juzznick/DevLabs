6750bc73d6f84ffac31add21e732188bd99ad77e
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-31 23:58:52 +0530

    activities

--- src/activities.jsx
+++ src/activities.jsx
@@ -1,6 +1,6 @@
 function Activities(){
     return(
-        <div>   
+        <div className='h-screen'>   
             <h1 className="bold flex justify-center text-2xl pt-24">Activities</h1>
         </div>
         

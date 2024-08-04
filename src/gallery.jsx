5f9439928f2cadd3cccfa5fed4481d958911131c
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-08-04 23:05:04 +0530

    gallery

--- src/gallery.jsx
+++ src/gallery.jsx
@@ -1,6 +1,9 @@
 function Gallery(){
     return(
-        <h1 className="bold flex justify-center text-2xl pt-24">Gallery</h1>
+        <div className="flex flex-col items-center">
+            <h1 className="bold flex justify-center text-2xl pt-24">Gallery</h1>
+            <img className="flex w-64 justify-center" src='img1.png'/>
+        </div>
     );
 }
 export default Gallery

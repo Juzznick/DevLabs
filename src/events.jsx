a7bf84e9860d6e139ac81ea7c44ee62e52a38d62
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-08-05 23:25:17 +0530

    events

--- src/events.jsx
+++ src/events.jsx
@@ -3,9 +3,9 @@ function Events() {
         <div className='h-screen'>
             <h1 className="flex justify-center">Upcoming Events</h1>
             <div className="flex justify-around">
-                <div className="h-96 w-96 bg-white"></div>
-                <div className="h-96 w-96 bg-yellow-300"></div>
-                <div className="h-96 w-96 bg-white"></div>
+                <div className="h-96 w-96 bg-white"> <h1>Web Development</h1></div>
+                <div className="h-96 w-96 bg-yellow-300"><h1>JavaScript Workshop</h1></div>
+                <div className="h-96 w-96 bg-white"><h1>Profile Building</h1></div>
             </div>
         </div>
 

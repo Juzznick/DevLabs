c1e192cf39b4e5efdeb3d5793e9d12fdea1cd356
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-08-05 23:22:23 +0530

    events

--- src/events.jsx
+++ src/events.jsx
@@ -1,9 +1,14 @@
-function Events(){
-    return(
-        <div className='h-screen'>   
+function Events() {
+    return (
+        <div className='h-screen'>
             <h1 className="flex justify-center">Upcoming Events</h1>
+            <div className="flex justify-around">
+                <div className="h-96 w-96 bg-white"></div>
+                <div className="h-96 w-96 bg-yellow-300"></div>
+                <div className="h-96 w-96 bg-white"></div>
+            </div>
         </div>
-        
+
     );
 }
 export default Events
